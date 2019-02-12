1d5f804c88b8f773e4c8cb16c32a730df379bba5
Alicia Toshima and Jen Bruce <jen@>
2019-02-11 20:22:49 -0800

    add feline friends repo

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,27 @@ $(document).ready(function() {
     event.preventDefault();
 
     var person1Input = $("#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    var beverageInput = $("#beverage").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
+    var streetInput = $("input#street").val();
+    var cityInput= $("input#city").val();
+    var stateInput = $("#state").val();
+    var zipInput = $("input#zip").val();
+    var productInput = $("#product").val();
+    var color = $("input:radio[name=color]:checked").val();
 
 
 
 
     $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-    $(".beverage").text(beverageInput);
-    $(".flavor").text(flavor);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+    $(".product").text(productInput);
+    $(".color").text(color);
 
-    $("#story").show();
+    $("#order").show();
 
-    // $("input:radio[name=flavor]:checked").children("value")first();
+
 
   });
 });
